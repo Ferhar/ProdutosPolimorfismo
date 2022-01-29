da632ec4ba21a89de6e0c560880e143a0ced22cd
Ferhar <git config --global user.email>
2022-01-29 18:08:40 -0300

    Exemplo de polimorfismo

---
+++ ProdutosPolimorfismo/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using ProdutosPolimorfismo.Entities;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace ProdutosPolimorfismo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            Console.Write("Enter the number of products: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i=1; i<= n; i++)
+            {
+                Console.WriteLine($"Product #{i} data:");
+                Console.Write("Common, used or imported (c/u/i)?");
+                char op = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Price: ");
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                if(op == 'i')
+                {
+                    Console.Write("Customs fee: ");
+                    double customsFee = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new ImportedProduct(name, price, customsFee));
+                }
+                else if(op == 'u')
+                {
+                    Console.Write("Manufacture date (DD/MM/YYYY): ");
+                    DateTime manufactureDate = DateTime.Parse(Console.ReadLine());
+                    list.Add(new UsedProduct(name, price, manufactureDate));
+                }
+                else
+                {
+                    list.Add(new Product(name, price));
+                }
+
+            }
+
+            Console.WriteLine("\nPRICE TAGS:");
+            foreach (Product p in list)
+            {
+                Console.WriteLine(p.PriceTag());
+            }
+        }
+    }
+}
